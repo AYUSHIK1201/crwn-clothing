4046d7f875faeada31caddae6fd25401659e9936
AYUSHIK1201 <en19cs301091@medicaps.ac.in>
2021-08-16 20:20:13 +0530

    update our menu-item tonavigate withRoute HOC

--- src/components/menu-item/menu-item.componenet.jsx
+++ src/components/menu-item/menu-item.componenet.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.style.scss';
-const MenuItem =({title,imageUrl,size})=>(
+const MenuItem =({title,imageUrl,size,history,linkUrl,match})=>(
     <div
-        className={`${size} menu-item`}>
+        className={`${size} menu-item`}
+        onClick={()=> history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image'
             style={{ 
                 backgroundImage:`url(${imageUrl})`
             }}/>
     <div className='content'>
         <h1 className='title'> {title.toUpperCase()}</h1>
-        <span className='subtite'> SHOP NOW</span>
+        <span className='subtitle'> SHOP NOW</span>
 
     </div>
 
 </div>
-);
+); 
 
-export default MenuItem;
+export default withRouter(MenuItem);
